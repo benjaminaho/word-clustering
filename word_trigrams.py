643c0e0676e4f5b9460e570a03cb245fb1263afc
benjaminaho <59031365+benjaminaho@users.noreply.github.com>
2020-11-01 17:51:04 -0500

    Create word-trigrams dictionary from corpus

---
+++ word_trigrams.py
@@ -0,0 +1,31 @@
+import os, nltk, json
+
+# Derive 1-, 2-, and 3-grams (2- and 3-grams include start and end char) from token.
+def trigramize(orgstr):
+    ans = [l for l in orgstr]
+    altstr = ' ' + orgstr + ' '
+    for i in range(1, len(altstr)-1):
+        ans.append(altstr[i-1:i+1])
+        ans.append(altstr[i-1:i+2])
+    ans.append(altstr[-2] + ' ')
+    return ans
+
+pattern = r'''(?x) (?:[A-Z]\.)+ | \w+(?:-\w+)* | \$?\d+(?:\.\d+)?%?'''
+
+# Create dictionary of token : list of trigrams pairs.
+
+dictionary = dict()
+
+for textfn in os.listdir(os.getcwd() + "/texts"):
+    with open("texts/" + textfn, "r") as textf:
+        for token in nltk.regexp_tokenize(textf.read().lower(), pattern):
+            if token not in dictionary:
+                dictionary[token] = trigramize(token)
+
+# Save dictionary to file.
+json.dump(dictionary, open("dictionary.json", "w"))
+
+
+#print(dictionary["london"])
+#print(dictionary["a"])
+#print(dictionary["ask"])
