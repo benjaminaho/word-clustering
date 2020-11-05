71f8dc668b8d25e8fbda2544e0604dd9a59af53e
benjaminaho <59031365+benjaminaho@users.noreply.github.com>
2020-11-04 20:10:05 -0500

    Added script which makes dictionary of (gram : list of grams following gram sorted by word frequency) pairs

---
+++ char_collocations_setup.py
@@ -0,0 +1,44 @@
+import json
+
+word_index = json.load(open("dictionary.json", "r"))
+gram_index = json.load(open("dictionary_inverse.json", "r"))
+
+following_grams = dict()
+
+for gram in gram_index:
+    next_grams = dict()
+    if len(gram) == 1:
+        continue
+    if len(gram) == 2:
+        for word in gram_index[gram]:
+            idx = word_index[word].index(gram)
+            if idx < len(word) * 3 - 4: # three-char strings
+                nxg = word_index[word][int((idx-len(word))/2)+1]
+                if nxg not in next_grams:
+                    next_grams[nxg] = 1
+                else:
+                    next_grams[nxg] += 1
+            if idx < len(word) * 3 - 6: # four-char strings
+                nxg = word_index[word][idx+4]
+                if nxg not in next_grams:
+                    next_grams[nxg] = 1
+                else:
+                    next_grams[nxg] += 1
+    elif len(gram) == 3:
+        for word in gram_index[gram]:
+            idx = word_index[word].index(gram)
+            if idx < len(word) * 3 - 7: # five-char strings
+                nxg = word_index[word][idx+5]
+                if nxg not in next_grams:
+                    next_grams[nxg] = 1
+                else:
+                    next_grams[nxg] += 1
+            if idx < len(word) * 3 - 9: # six-char strings
+                nxg = word_index[word][idx+6]
+                if nxg not in next_grams:
+                    next_grams[nxg] = 1
+                else:
+                    next_grams[nxg] += 1
+    following_grams[gram] = sorted(next_grams.items(), key=lambda x: x[1], reverse=True)
+
+json.dump(following_grams, open("following_grams.json", "w"))
